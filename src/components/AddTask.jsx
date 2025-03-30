38927404dd4bf9d810c7b7af518b745a5729a20b
Abhishek Maurya <maurya4122002@gmail.com>
2025-03-30 23:57:05 +0530

    Your commit message

--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -88,12 +88,13 @@ const AddTask = () => {
                                     Description
                                 </label>
                                 <textarea
-                                    className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                                    className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500 min-h-[120px]"
                                     id="description"
                                     placeholder="Task Description"
                                     name="description"
                                     value={formData.description}
                                     onChange={handleChange}
+                                    rows={5}
                                 />
                             </div>
                         </div>
